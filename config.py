57255bc33320d4b5574f188f9840f0c398442a31
looserakki <70113298+looserakki@users.noreply.github.com>
2021-07-13 08:24:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,35 +1,18 @@
-# Calls Music 1 - Telegram bot for streaming audio in group calls
-# Copyright (C) 2021  Roj Serbest
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# Modified by Inukaasith 
-
 from os import getenv
 
 from dotenv import load_dotenv
 
 load_dotenv()
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
+SESSION_NAME = getenv("SESSION_NAME", "BQAOSxIXTcYySfkWKac5GH6vOtTyUbYd2M76QHmeP0W5HzBLIMkPI8tv4bvmBChxE1cKSm3UsBmOxDXGPE0ia0gW0KTYuiWCzWXffYD2F0Jsb8SHShUGvKx81wwBWpifo086IunLkVFb0SgjU5NTPQ7_h6w2NsZxfxTIUo6YgaaZz46rda_FQj6xb_xzV8nU7MH7aryl1m6ZhlRUVd9HirsYG7PJ8-RAyCf-s6UEt3s1YVMMCYtC54gm3_qTiF3ivT7K0f4JbuiRkilveiPninfU9WLA_oAO3nLyqgXu8wQ4nTm3DvcTSZzg-1tMpBnMLy4g6-kn1BXIUleYL6FLn7vrb6crEAA")
+BOT_TOKEN = getenv("BOT_TOKEN", "1728730929:AAGdj_UpflyimraBPr-MRHi8PtVMwKZMWr4")
+BOT_NAME = getenv("BOT_NAME", "PATRICIA")
 admins = {}
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "5786603"))
+API_HASH = getenv("API_HASH", "a1354f206a4a05109d0fc916c0f150d0")
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1832447570").split()))
